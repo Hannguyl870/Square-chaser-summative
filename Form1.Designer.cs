f6fb3fa7b47badf85ad54ee3e36d76d210b0a8e6
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-11-23 11:29:17 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Square_chaser_summative
+{
+    partial class Squarechaser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Squarechaser));
+            this.scorelable = new System.Windows.Forms.Label();
+            this.loselable = new System.Windows.Forms.Label();
+            this.gamertimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // scorelable
+            // 
+            this.scorelable.ForeColor = System.Drawing.Color.LawnGreen;
+            this.scorelable.Location = new System.Drawing.Point(338, 17);
+            this.scorelable.Name = "scorelable";
+            this.scorelable.Size = new System.Drawing.Size(100, 23);
+            this.scorelable.TabIndex = 0;
+            this.scorelable.Text = "0";
+            this.scorelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loselable
+            // 
+            this.loselable.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loselable.ForeColor = System.Drawing.Color.Red;
+            this.loselable.Location = new System.Drawing.Point(318, 180);
+            this.loselable.Name = "loselable";
+            this.loselable.Size = new System.Drawing.Size(149, 59);
+            this.loselable.TabIndex = 1;
+            // 
+            // gamertimer
+            // 
+            this.gamertimer.Enabled = true;
+            this.gamertimer.Interval = 20;
+            this.gamertimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Squarechaser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loselable);
+            this.Controls.Add(this.scorelable);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Squarechaser";
+            this.Text = "Square Chaser";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Squarechaser_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scorelable;
+        private System.Windows.Forms.Label loselable;
+        private System.Windows.Forms.Timer gamertimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Square_chaser_summative
+{
+    public partial class Squarechaser : Form
+    {
+        int playerScore = 0;
+
+        int playerSpeed = 4;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool LDown = false;
+        bool RDown = false;
+        bool leftarrowdown = false;
+        bool rightarrowdown = false;
+
+        Rectangle player = new Rectangle(280, 200, 30, 30);
+        Rectangle redsquare = new Rectangle(110, 195, 15, 15);
+        Rectangle greensquare = new Rectangle(200, 110, 10, 10);
+
+        SolidBrush lightblueBrush = new SolidBrush(Color.LightBlue);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush greenyBrush = new SolidBrush(Color.GreenYellow);
+
+        Random randsquares = new Random();
+
+        public Squarechaser()
+        {
+            InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.L:
+                    LDown = true;
+                    break;
+                case Keys.R:
+                    RDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.R:
+                    RDown = false;
+                    break;
+                case Keys.L:
+                    LDown = false;
+                    break;
+            }
+        }
+
+        private void Squarechaser_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(lightblueBrush, player);
+            e.Graphics.FillRectangle(redBrush, redsquare);
+            e.Graphics.FillRectangle(greenyBrush, greensquare);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //move ball 
+            
+            
+            if (upArrowDown == true)
+            {
+                player.Y -= playerSpeed;
+            }
+            else if (downArrowDown == false)
+            {
+                player.Y += playerSpeed;
+            }
+
+            //check if ball hit top or bottom wall and change direction if it does 
+            if (player.X < 0 || player.X > this.Height - player.Height)
+            {
+                playerSpeed *= -1;  // or: ballYSpeed = -ballYSpeed; 
+            }
+            //move player 
+            if (wDown == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (sDown == true && player.X < this.Height - player.Height)
+            {
+                player.X += playerSpeed;
+            }
+            if (playerScore < 0)
+            {
+                gamertimer.Enabled = false;
+                loselable.Visible = true;
+                loselable.Text = "You lose";
+
+            }
+
+            Refresh();  
+
+        }
+    }
+}
